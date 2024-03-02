8088b85e196085b03567a308ac06c2d27cf504c6
antrisk40 <neeleshbaghel40@gmail.com>
2024-03-02 22:42:58 +0530

    added store redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Home from '../pages/Home'
-import SignIn from '../pages/SignIn'
-import SignUp from '../pages/SignUp'
-import About from "../pages/about";
-import Profile from '../pages/Profile'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from "./pages/about";
+import Profile from './pages/Profile'
 import Header from './components/Header';
 
 
