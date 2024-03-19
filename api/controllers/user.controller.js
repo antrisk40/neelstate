607762c3b28f5f2364b5d6c57c58b92d8c9c04f3
antrisk40 <neeleshbaghel40@gmail.com>
2024-03-19 17:05:45 +0530

    create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -43,4 +43,19 @@ export const deleteUser = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    return next(errorHandler(401, 'you can only view your own Listings'));
+  }
 }
+
+
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector} from 'react-redux';
-import { signInStart, signInFailure, signInSuccess } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 import OAuth from "../components/OAuth";
 
-function SignIn() {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
@@ -16,7 +20,7 @@ function SignIn() {
     });
   };
   const handleSubmit = async (e) => {
-    e.preventDefault(); //use to prevent refresh of page
+    e.preventDefault();
     try {
       dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
@@ -38,10 +42,9 @@ function SignIn() {
       dispatch(signInFailure(error.message));
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center my-7">Sign In</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="email"
@@ -57,23 +60,22 @@ function SignIn() {
           id="password"
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
-        <OAuth/>
+        <OAuth />
       </form>
-      <div className=" flex gap-2 mt-5">
-        <p>Don't have an account ?</p>
+      <div className="flex gap-2 mt-5">
+        <p>Dont have an account?</p>
         <Link to={"/sign-up"}>
-          <span className="text-blue-700">Sign Up</span>
+          <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
-
-export default SignIn;
